dfe0bce44defd82fe15c860e87e6846354303f83
pavelrobot <pavel@rodiniageneration.io>
2021-04-08 22:47:19 +0200

    Controller and Test

---
+++ DanskeBankTest/Controllers/LoanController.cs
@@ -0,0 +1,19 @@
+﻿using DanskeBankTest.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DanskeBankTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoanController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Loan> Repay(decimal totalAmountRepaid, int loanTerm, decimal totalDebt)
+        {
+            return NotFound();
+        }
+    }
+}
---
+++ DanskeBankTest/Model/LoanRepayDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace DanskeBankTest.Model
+{
+    public class LoanRepayDTO
+    {
+        public decimal AmountRepaid { get; set; }
+        public int LoanTerm { get; set; } 
+        public decimal TotalDebt { get; set; }
+    }
+}
